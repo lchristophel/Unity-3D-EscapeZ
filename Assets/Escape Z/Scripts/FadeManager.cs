b1912db0074806bf6025d140a5c4df16f73887c5
unknown <grebert62231@gmail.com>
2023-10-09 13:49:21 +0200

    + scenes fade manager

---
+++ Assets/Escape Z/Scripts/FadeManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FadeManager : MonoBehaviour
+{
+    public Image fade; // Image utilisée pour le fondu
+    public float fadeInDuration = 5.0f; // Durée du fondu entrant en secondes
+    public float fadeOutDuration = 3.0f; // Durée du fondu sortant en secondes
+
+    private bool isFading = false;  // Indique si le fondu est en cours
+
+    void Start()
+    {
+        // Démarrer le fondu pour la première scène
+        FadeIn();
+    }
+
+    // Fondu sortant
+    public void FadeOut()
+    {
+        fade.CrossFadeAlpha(1.0f, fadeOutDuration, false);
+        isFading = true;
+
+        // Charger la nouvelle scène après le fondu sortant
+        Invoke("LoadNextScene", fadeOutDuration);
+    }
+
+    // Fondu entrant
+    public void FadeIn()
+    {
+        fade.CrossFadeAlpha(0.0f, fadeInDuration, false);
+        isFading = true;
+
+        // Réinitialiser le statut du fondu après le fondu entrant
+        Invoke("ResetFadingStatus", fadeInDuration);
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    void ResetFadingStatus()
+    {
+        isFading = false;
+    }
+
+    void Update()
+    {
+        if (isFading)
+            return;
+
+        // Exemple : appuyez sur "Espace" pour déclencher le fondu sortant
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            FadeOut();
+        }
+    }
+}
--- Assets/Escape Z/Scripts/MainMenu.cs
+++ Assets/Escape Z/Scripts/MainMenu.cs
@@ -5,13 +5,6 @@ public class MainMenu : MonoBehaviour
 {   
     void Update()
     {
-
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            // Charger la scène du jeu (à adapter avec le nom de la scène ou le numéro)
-            SceneManager.LoadScene(1);
-        }
-
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             // Fonction pour quitter l'application
